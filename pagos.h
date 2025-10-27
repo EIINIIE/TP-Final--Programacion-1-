b2ddd3a81b1257d286a473934a0e3828d1b2623a
EIINIIE <165053135+EIINIIE@users.noreply.github.com>
2025-10-26 22:40:48 -0300

    agregue y falta ver si anda
    faltaría ubicar las funciones

--- pagos.h
+++ pagos.h
@@ -1,6 +1,19 @@
 #ifndef PAGOS_H_INCLUDED
 #define PAGOS_H_INCLUDED
 
+#include "fecha.h"
 
+// ------- Estructura Empleado -------
+typedef struct stPago
+{
+    int idPago;                // ID único del pago
+    char dniCliente[15];       // Cliente que realiza el pago
+    float monto;
+    Fecha fechaPago;
+    char medioPago[20];        // Efectivo, tarjeta, transferencia, etc.
+} Pago;
+
+// ------- Prototipo -------
+float medioDPago(float precioBase);
 
 #endif // PAGOS_H_INCLUDED
--- venta.h
+++ venta.h
@@ -2,6 +2,12 @@
 #define VENTA_H_INCLUDED
 #include "fecha.h"
 
+/// faltaria implementarlo
+
+#define ARCHIVO_AUTOS "autos.dat"
+#define ARCHIVO_CLIENTES "clientes.dat"
+#define ARCHIVO_VENTAS "ventas.dat"
+
 // ------- Estructura Venta -------
 typedef struct stVenta
 {
@@ -14,7 +20,10 @@ typedef struct stVenta
 } Venta;
 
 // ------- Prototipo -------
-
+Venta cargarVenta();
+void registrarVenta();
+void mostrarVenta(Venta ventas);
+void mostrarVentas();
 
 #endif // VENTA_H_INCLUDED
 ///Esto va al final porque #endif es el cierre de una condición
